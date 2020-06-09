e58972c5367882103c09bcc779248e31ad6fe2ae
Jamil Ahmed <jamil.ahmed.ja28@gmail.com>
2020-06-10 01:53:17 +0500

    work done demo ready

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import './App.css';
+import {Cards,Chart,CountryPicker} from './components';
+import styles from './App.module.css';
+import{fetchData} from './api';
+import coranaImage from './images/image.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Bootcamp Covit-19 Project</h1>
-            <p>This is my First Test</p>
-            
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+
+  state = {
+    data:{},
+    country:''
+  }
+
+  async componentDidMount(){
+    const fetchedData = await fetchData()
+    this.setState({data: fetchedData})
+  }
+  handleCountryCahnge = async (country)=>{
+    const fetchedData = await fetchData(country)
+    this.setState({data: fetchedData,country:country})
+  }
+  render(){
+    const {data,country} = this.state;
+    return(
+      <div className={styles.container}>
+        <img src={coranaImage}/>
+        <Cards data = {data}/>
+        <CountryPicker handleCountryCahnge={this.handleCountryCahnge} />
+        <Chart data = {data} country={country}/>
+       
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,25 @@
+import React,{useState,useEffect} from 'react';
+import {fetchcontry} from '../../api';
+import { NativeSelect,FormControl} from '@material-ui/core';
+import styles from './CountryPicker.module.css';  
+
+const CountryPicker = ({handleCountryCahnge})=>{
+    const [fetchedCountries,setFetchCountries] = useState([]);
+    useEffect(()=>{
+        const fetchapi = async()=>{
+            setFetchCountries(await fetchcontry())
+        }
+        fetchapi()
+    },[setFetchCountries])
+
+    return(
+        <FormControl>
+            <NativeSelect defaultValue="" onChange={e =>handleCountryCahnge(e.target.value) }>
+                <option value="">Global</option>
+    {fetchedCountries.map((counrty,i)=><option key={i} value={counrty}>{counrty}</option>)}
+            </NativeSelect>
+        </FormControl>
+    )
+}
+
+export default CountryPicker;
